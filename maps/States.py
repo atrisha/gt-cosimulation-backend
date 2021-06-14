7f6de0319e410dd759a74455b864c64d2e2a6537
Atrisha Sarkar <atrisha.sarkar@uwaterloo.ca>
2021-06-14 13:56:08 -0400

    support for synthetic scenedef

--- maps/States.py
+++ maps/States.py
@@ -328,7 +328,8 @@ class SyntheticScenarioDef:
     
     def __init__(self,agent_id, agent_waypoints,agent_waypoint_segments, direction, file_id,initialize_db,start_ts,freq):
         assert len(agent_waypoints[0]) == 3, "Agent waypoints should contain (x,v,v) information"
-        agent_attribs = {'x':agent_waypoints[0][0], 'y':agent_waypoints[0][1], 'velocity':agent_waypoints[0][2], 'waypoints':agent_waypoints, 'file_time':start_ts, 'id':agent_id, 'waypoint_segments':agent_waypoint_segments, 'direction':direction}
+        waypoint_path = [(x[0],x[1]) for x in agent_waypoints]
+        agent_attribs = {'x':agent_waypoints[0][0], 'y':agent_waypoints[0][1], 'velocity':agent_waypoints[0][2], 'waypoints':waypoint_path, 'file_time':start_ts, 'id':agent_id, 'waypoint_segments':agent_waypoint_segments, 'direction':direction}
         self.agent = VehicleState(agent_attribs)
         
         
